fad3f353e482ddbfea73fe9e4633d17ac7e50967
carok76 <carolin.krahmer@gmail.com>
2026-01-07 14:05:55 +0100

    Updated links
    Links to GitHub and Netlify updated

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default function App() {
       <Search onSearch={handleSearch} />
       <Weather city={city} />
       <Forecast />
-      <p className="footer">This project was coded by <a href="https://www.linkedin.com/in/carolin-krahmer-94661399/" target="_blank" rel="noreferrer">Carolin Krahmer</a> and is on  <a href="https://github.com/carok76/Weather-App" target="_blank" rel="noreferrer">GitHub</a> and hosted on <a href="https://frolicking-bubblegum-45cfd5.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>.</p>
+      <p className="footer">This project was coded by <a href="https://www.linkedin.com/in/carolin-krahmer-94661399/" target="_blank" rel="noreferrer">Carolin Krahmer</a> and is on  <a href="https://github.com/carok76/react-weather-app/" target="_blank" rel="noreferrer">GitHub</a> and hosted on <a href="https://caros-react-weather-app.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>.</p>
     </div>
   );
 }
